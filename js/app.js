0c6efdefcd0a45025e6e40e35c0a8b0f90c496d5
JudyVue <judyvue87@gmail.com>
2016-08-16 18:17:09 -0700

    nothings working but just committing

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var projectsArray = [];
+
+function Project (options) {
+  this.title = options.title;
+  this.siteUrl = options.siteUrl;
+  this.repoUrl = options.repoUrl;
+  this.dateCreated = options.dateCreated;
+}
+
+Project.prototype.toHtml = function(){
+  var $newProject = $('section.projectlist').clone();
+
+  //find my elements for jquery objects
+  $newProject.find('.livesite').attr('href', this.siteUrl);
+  $newProject.find('.repo').attr('href', this.repoUrl);
+  $newProject.find('.aboutme').text(this.title);
+
+//display the #days the project was created
+  $newProject.find('time[pubdate]').attr('title', this.dateCreated);
+  $newProject.find('time').text('about ' + parseInt((new Date() - new Date(this.dateCreated))/60/60/24/1000) + ' days ago');
+
+//remove the class
+  $newProject.removeClass('projectlist');
+  return $newProject;
+};
+
+//sorts most recent project to top
+myProjects.sort(function(firstElement, secondElement) {
+  return (new Date(secondElement.dateCreated)) - (new Date(firstElement.dateCreated));
+});
+
+//pushing the results of instantiation sorts to new array
+myProjects.forEach(function(currentProject) {
+  projectsArray.push(new Project(currentProject));
+});
+
+//attaches to section in DOM
+projectsArray.forEach(function(project) {
+  $('#projectlist').append(project.toHtml());
+});
--- userstories.md
+++ userstories.md
@@ -5,3 +5,10 @@
 * As the creator, I need the page to link to my social and GitHub pages, so that visitors can follow me, and I can build my audience. In addition, I'd like a few of my headshots featured on the site, and I'm still debating if I want to link to my standup comedy.
 
 * As a visitor, I want responsive design so I can view the site easily on any device without being annoyed. I want a clean setup that lets me know clearly who Judy Vue and why she is awesome.
+
+*As a developer, I want my site to use valid and semantic markup, so that employers will love me.
+As the creator, I want the page to link to my social and GitHub pages, so that visitors can follow me, and I can build my audience.
+
+*As a developer, I want portfolio items displayed with a repeatable template, so that I can reuse it, and abstract out the details for individual projects.
+
+*As a developer, I want my CSS organized (think SMACSS) so that I can find my stylings swiftly.
