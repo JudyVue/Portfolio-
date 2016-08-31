cc7356169a03f2e641029566c6856d1f299d06ae
JudyVue <judyvue87@gmail.com>
2016-08-31 16:00:45 -0700

    added second ajax function

--- model/repo.js
+++ model/repo.js
@@ -3,6 +3,7 @@
 
 
   reposObject.allRepos = [];
+  reposObject.followers = [];
 
  //AJAX call to Github API
   reposObject.requestRepos = function(callback) {
@@ -11,10 +12,16 @@
       .done(function(data){
         reposObject.allRepos = data;
         console.log(reposObject.allRepos);
-        callback();
-      }));
+      }),
+      $.get('/github/users/JudyVue/followers')
+      .done(function(data){
+        reposObject.followers = data;
+        console.log(reposObject.followers);
+      })
+    ).done(callback);
   };
 
+// https://api.github.com/users/JudyVue/followers
 
 
   module.reposObject = reposObject;
