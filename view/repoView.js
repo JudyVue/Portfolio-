cc94ae4fa4852e3d034bc4f003e430dea285ebe2
JudyVue <judyvue87@gmail.com>
2016-08-31 16:18:31 -0700

    commit

--- view/repoView.js
+++ view/repoView.js
@@ -5,6 +5,7 @@
 
   repoView.renderRepos = function() {
     $('#aboutmesection').append(repoCompiler(reposObject.allRepos)); // reposObject.allRepos is already an object, so just need to run Handlebars compile method on the object
+    $('#aboutmesection').append(reposObject.followers.map(repoCompiler));
   };
 
   reposObject.requestRepos(repoView.renderRepos);
