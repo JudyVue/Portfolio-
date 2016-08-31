d3fe312987a8af671dc8b2e172103541b462e4fc
JudyVue <judyvue87@gmail.com>
2016-08-31 14:52:24 -0700

    commiting in order to deploy via heroku

--- model/repo.js
+++ model/repo.js
@@ -4,19 +4,14 @@
 
   reposObject.allRepos = [];
 
-
+ //AJAX call to Github API
   reposObject.requestRepos = function(callback) {
-    console.log('hello');
-    $.ajax({
-      url: 'https://api.github.com/users/JudyVue',
-      type: 'GET',
-      headers: {'Authorization': 'token ' + githubToken },
-      success: function(data) {
+    $.when(
+      $.get('/github/users/JudyVue')
+      .done(function(data){
         reposObject.allRepos = data;
-        console.log(reposObject.allRepos);
-        callback();
-      }
-    });
+      })
+    ).done(callback);
   };
 
 
--- view/repoView.js
+++ view/repoView.js
@@ -5,7 +5,6 @@
 
   repoView.renderRepos = function() {
     $('#aboutmesection').append(repoCompiler(reposObject.allRepos)); // reposObject.allRepos is already an object, so just need to run Handlebars compile method on the object
-    console.log('testing');
   };
 
   reposObject.requestRepos(repoView.renderRepos);
